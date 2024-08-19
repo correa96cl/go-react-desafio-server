187020d962f035ff2c81dcb11154b9aec63fc31c
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2024-08-19 19:51:59 -0300

    Commit do backend do desafio em go

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # go-react-desafio
+
+docker-compose down -v 
+
+
+go mod tidy
+
+
+sqlc generate -f ./internal/store/pgstore/sqlc.yaml
+
+go generate ./...
--- cmd/tools/terndotenv/main.go
+++ cmd/tools/terndotenv/main.go
@@ -1,25 +1,33 @@
 package main
 
 import (
+	"log"
 	"os/exec"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
+
 	if err := godotenv.Load(); err != nil {
-		panic(err)
+		log.Fatal("Erro ao carregar variáveis de ambiente:", err)
 	}
 
 	cmd := exec.Command(
 		"tern",
 		"migrate",
 		"--migrations",
-		"./internal/store/pgstore/migrations",
+		"./internal/store/pgstore/migrations", // Replace with your migrations directory
 		"--config",
 		"./internal/store/pgstore/migrations/tern.conf",
 	)
+
 	if err := cmd.Run(); err != nil {
-		panic(err)
+		log.Printf("Erro ao executar o comando: %s\n", err)
+		log.Printf("Comando executado: %s\n", cmd.String())
+	} else {
+		log.Println("Variáveis de ambiente carregadas com sucesso!")
+
 	}
+
 }
